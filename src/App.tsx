955d028c7e68b6eae8e119e17e5a013c373bf3a5
Illia Ptah <ilyaptakh@gmail.com>
2024-01-22 17:36:02 +0200

    added phone page grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import './App.css';
+import { PhonesPageGrid } from './components/PhonesPageGrid/PhonesPageGrid';
 
 function App() {
   return (
     <div className="App">
       <h1>Product Catalog</h1>
+      <PhonesPageGrid />
     </div>
   );
 }
---
+++ src/components/PhonesPageGrid/PhonesPageGrid.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './container.scss';
+import './card.scss';
+
+export const PhonesPageGrid = () => (
+
+  <div className="container">
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+    <div className="card" />
+  </div>
+
+);
+
+export default PhonesPageGrid;
