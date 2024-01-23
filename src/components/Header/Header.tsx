25b1031637e2b8a1526c2af9f309bf1f7b1d0563
Svitlana <svitlana.ponomarchuk@gmail.com>
2024-01-24 00:20:18 +0200

    added burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,84 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
+import cn from 'classnames';
+import { useLocation } from 'react-router-dom';
 import { NavList } from '../NavList';
 import { Logo } from '../Logo';
 import { NavItem } from '../NavItem';
+import { BtnMenu } from '../BtnMenu';
 import { ReactComponent as FavouritesIcon } from '../../images/icons/favourites.svg';
 import { ReactComponent as ShopBagIcon } from '../../images/icons/shopping-bag.svg';
 import { useWindowWidth } from '../../hooks/useWindowWidth';
 import styles from './Header.module.scss';
 
 export const Header: FC = () => {
-  const isNotMob = useWindowWidth() > 600;
+  const [showMobMenu, setShowMobMenu] = useState(false);
+  const isNotMob = useWindowWidth() >= 640;
+  const location = useLocation();
+
+  useEffect(() => {
+    setShowMobMenu(false);
+  }, [location]);
+
+  useEffect(() => {
+    if (showMobMenu && !isNotMob) {
+      document.body.style.overflow = 'hidden';
+    } else if (!showMobMenu || isNotMob) {
+      document.body.style.overflow = 'unset';
+    }
+  }, [showMobMenu, isNotMob]);
 
   return (
-    <header className={styles.header}>
-      <div className={styles.header__nav}>
-        <Logo />
-        {isNotMob && <NavList />}
-      </div>
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__nav}>
+          <Logo />
+          {isNotMob && <NavList />}
+        </div>
 
-      {isNotMob && (
-        <div className={styles.header__icons}>
-          <NavItem href="/" isIcon>
+        {!isNotMob && (
+          <BtnMenu onclick={setShowMobMenu} onClose={!showMobMenu} />
+        )}
+
+        {isNotMob && (
+          <div className={styles.header__icons}>
+            <NavItem href="/" isIcon count={0}>
+              <FavouritesIcon />
+            </NavItem>
+
+            <NavItem href="/shop" isIcon count={3}>
+              <ShopBagIcon />
+            </NavItem>
+          </div>
+        )}
+      </header>
+
+      <div
+        className={cn(styles['header__mob-menu'], {
+          [styles['header__mob-menu--show']]: showMobMenu && !isNotMob,
+        })}
+      >
+        <NavList toColumn />
+
+        <div className={styles['header__mob-menu-footer']}>
+          <NavItem
+            href="/"
+            isIcon
+            count={110}
+            onMobMenu
+          >
             <FavouritesIcon />
           </NavItem>
 
-          <NavItem href="/shop" isIcon count={3}>
+          <NavItem
+            href="/shop"
+            isIcon
+            count={3}
+            onMobMenu
+          >
             <ShopBagIcon />
           </NavItem>
         </div>
-      )}
-    </header>
+      </div>
+    </>
   );
 };
