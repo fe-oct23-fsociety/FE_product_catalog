285c53470cd244172cd37d0c72feeed128ee2a08
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-01-23 16:28:43 +0200

    fixed merge conflicts

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 import styles from './Logo.module.scss';
 
-export const Logo: FC = () => {
+export const Logo: React.FC = () => {
   return (
     <h1 className={styles.logo}>
       <NavLink to="/">
