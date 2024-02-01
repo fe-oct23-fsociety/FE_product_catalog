bd5ce124e93664803278ea2334e57c434c8cef5b
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-02-01 21:08:32 +0200

    got rid from slider bullets on burger-menu

--- src/components/ProductsPageGrid/ProductsPageGrid.tsx
+++ src/components/ProductsPageGrid/ProductsPageGrid.tsx
@@ -88,7 +88,6 @@ export const ProductsPageGrid: React.FC<Props> = ({
     setTimeout(() => {
       onSortBySelect(event.target.value);
       setIsDelayActive(false);
-      handleSortParamsChange();
     }, DELAY_TIME);
   };
 
@@ -99,7 +98,6 @@ export const ProductsPageGrid: React.FC<Props> = ({
     setTimeout(() => {
       onSortTypeSelect(event.target.value);
       setIsDelayActive(false);
-      handleSortParamsChange();
     }, DELAY_TIME);
   };
 
